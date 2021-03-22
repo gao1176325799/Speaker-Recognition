b5aec7bea7da76d0ba70d397938e1d72b028060b
Victor-Gao <gxh.vic@gmail.com>
2021-03-22 01:15:28 -0300

    model created successfully

--- main.py
+++ main.py
@@ -250,3 +250,54 @@ valid_ds = valid_ds.map(
     lambda x, y: (audio_to_fft(x), y), num_parallel_calls=tf.data.experimental.AUTOTUNE
 )
 valid_ds = valid_ds.prefetch(tf.data.experimental.AUTOTUNE)
+
+#model definition
+def residual_block(x, filters, conv_num=3, activation="relu"):
+    # Shortcut
+    s = keras.layers.Conv1D(filters, 1, padding="same")(x)
+    for i in range(conv_num - 1):
+        x = keras.layers.Conv1D(filters, 3, padding="same")(x)
+        x = keras.layers.Activation(activation)(x)
+    x = keras.layers.Conv1D(filters, 3, padding="same")(x)
+    x = keras.layers.Add()([x, s])
+    x = keras.layers.Activation(activation)(x)
+    return keras.layers.MaxPool1D(pool_size=2, strides=2)(x)
+
+
+def build_model(input_shape, num_classes):
+    inputs = keras.layers.Input(shape=input_shape, name="input")
+
+    x = residual_block(inputs, 16, 2)
+    x = residual_block(x, 32, 2)
+    x = residual_block(x, 64, 3)
+    x = residual_block(x, 128, 3)
+    x = residual_block(x, 128, 3)
+
+    x = keras.layers.AveragePooling1D(pool_size=3, strides=3)(x)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(256, activation="relu")(x)
+    x = keras.layers.Dense(128, activation="relu")(x)
+
+    outputs = keras.layers.Dense(num_classes, activation="softmax", name="output")(x)
+
+    return keras.models.Model(inputs=inputs, outputs=outputs)
+
+
+model = build_model((SAMPLING_RATE // 2, 1), len(class_names))
+
+model.summary()
+
+# Compile the model using Adam's default learning rate
+model.compile(
+    optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
+
+# Add callbacks:
+# 'EarlyStopping' to stop training when the model is not enhancing anymore
+# 'ModelCheckPoint' to always keep the model that has the best val_accuracy
+model_save_filename = "model.h5"
+
+earlystopping_cb = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+mdlcheckpoint_cb = keras.callbacks.ModelCheckpoint(
+    model_save_filename, monitor="val_accuracy", save_best_only=True
+)
